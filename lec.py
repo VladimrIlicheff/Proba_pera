6686249ca4220a5993afc1869830141d6d791f44
Vladimir <user@MAK.local>
2022-12-17 20:11:42 +0700

    proba

--- lec.py
+++ lec.py
@@ -1,7 +1,14 @@
-print("x y z w") 
-for x in range(2):
-  for y in range(2):
-   for z in range(2):
-    for w in range(2):
-      if not ((w and z) or not y or (not x == (not w))):
-        print(x, y, z, w)
+# print("x y z w") 
+# for x in range(2):
+#   for y in range(2):
+#    for z in range(2):
+#     for w in range(2):
+#       if not ((w and z) or not y or (not x == (not w))):
+#         print(x, y, z, w)
+n = int(input())
+m = int(input())
+
+if n == m ** 2 or m == n ** 2:
+ print("Yes")
+else:
+ print("No")
